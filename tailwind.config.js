7ba23ab9f1cd65a941245a7d4e826ea16a340d56
Erfanmah7 <efn8mah@yahoo.com>
2024-09-20 16:46:28 +0330

    start project speed-vpn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./public/**/*.{html,js}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Dana: "Dana",
+        DanaDemiBold: "Dana DemiBold",
+        DanaMedium: "Dana Medium",
+        MorabbaLight: "Morabba Light",
+        MorabbaMedium: "Morabba Medium",
+        MorabbaBold: "Morabba Bold",
+      },
+
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          md: "4rem",
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({ addVariant }) {
+      addVariant("child", "& > *");
+      addVariant("child-hover", ["& > *:hover"]);
+    },
+  ],
 };
