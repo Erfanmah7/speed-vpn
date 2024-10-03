4d7959ee89181f4bf481cdf51041e574373fb6d8
Erfanmah7 <efn8mah@yahoo.com>
2024-10-03 17:16:55 +0330

    add darkmode

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 function Header() {
+  const [mod, setMod] = useState(localStorage.getItem("theme") === "light");
+
+  useEffect(() => {
+    toggleDarkMod();
+  }, [mod]);
+  const toggleDarkMod = () => {
+    if (!mod) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  };
+
   return (
     <div className="w-[1322px] h-[89px] bg-[#081A30] rounded-[15px] flex justify-between items-center px-[23px] py-[26px] mb-[21px]">
       <Link to="/">logo</Link>
-      <div className="flex gap-x-[67.50px]">
+      <div className="flex gap-x-[67.50px] items-center">
         <div>
           <ul className="flex gap-x-[23px]">
             <Link to="/installationguide">
@@ -19,7 +34,18 @@ function Header() {
             </Link>
           </ul>
         </div>
-        <div>profile</div>
+        <div>
+          {/* dark mode */}
+          <div className="cursor-pointer" onClick={() => setMod(!mod)}>
+            <svg className="h-[43px] w-[43px]  dark:text-white text-black inline-block dark:hidden">
+              <use href="#moon-icon"></use>
+            </svg>
+
+            <svg className="h-[43px] w-[43px] dark:text-white text-black  hidden dark:inline-block">
+              <use href="#sun-icon"></use>
+            </svg>
+          </div>
+        </div>
       </div>
     </div>
   );
