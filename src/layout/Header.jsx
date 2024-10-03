5336c39bc61157acab33554bf99f9e58df69c614
Erfanmah7 <efn8mah@yahoo.com>
2024-10-03 16:59:30 +0330

    add guide

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <div className="w-[1322px] h-[89px] bg-[#081A30] rounded-[15px] flex justify-between items-center px-[23px] py-[26px] mb-[21px]">
-      <div>logo</div>
+      <Link to="/">logo</Link>
       <div className="flex gap-x-[67.50px]">
         <div>
           <ul className="flex gap-x-[23px]">
--- src/pages/InstallationGuide.jsx
+++ src/pages/InstallationGuide.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 function InstallationGuide() {
-  return <div>InstallationGuide</div>;
+  return (
+    <div className="h-[964px] w-[1322px] bg-[#081A30] rounded-[15px]">
+      <div>
+        <h1>برنامه نصبی مربوط به هر سیستم عامل</h1>
+        <p>
+          با استفاده از منو های زیر نسبت به سیستم عامل خود می توانید از اشتراک
+          خود استفاده کنید.
+        </p>
+        <p>نکته: اتصال در مک مشابه ios است</p>
+      </div>
+      <div>next</div>
+    </div>
+  );
 }
 
 export default InstallationGuide;
